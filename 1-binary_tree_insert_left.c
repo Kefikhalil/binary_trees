580befcc7d73c305a1ab595ffd3dbb0d410c4270
Kefikhalil <naroqt@gmail.com>
2020-07-23 16:57:04 -0700

    updated

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,21 +10,21 @@
 
  binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *node;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
 	{
-		new->left = parent->left;
-		parent->left->parent = new;
+		node->left = parent->left;
+		parent->left->parent = node;
 	}
-	parent->left = new;
+	parent->left = node;
 
-	return (new);
+	return (node);
 }
