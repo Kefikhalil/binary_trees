3a115a8b731e1be984f070f7c2973d0220ab16e5
Kefikhalil <naroqt@gmail.com>
2020-07-24 15:29:32 -0700

    updated

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * maximum - returns the maximum of two integers
+ * @first: first integer
+ * @second: second integer
+ *
+ * Return: 0
+ */
+
+ size_t binary_tree_height(const binary_tree_t *tree)
