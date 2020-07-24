4eee99ba5411752388f7537a89b12783b40c2f8c
Kefikhalil <naroqt@gmail.com>
2020-07-24 15:18:30 -0700

    updated

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,12 +9,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
-				return;
-			binary_tree_delete(tree->left);
-			binary_tree_delete(tree->right);
-
-		free(tree);
-		tree = NULL;
-
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+	tree = NULL;
 }
