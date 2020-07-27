8a6cfaf7a59473fceb6d8de61e67277417d033d4
Kefikhalil <naroqt@gmail.com>
2020-07-27 16:11:08 +0000

    Correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,5 +8,24 @@
  * Return: 0
  */
 
- binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *tmp;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	tmp = malloc(sizeof(binary_tree_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->n = value;
+	tmp->parent = parent;
+	if (parent->left != NULL)
+	{
+		tmp->left = parent->left;
+		tmp->left->parent = tmp;
+	}
+	parent->left = tmp;
+	return (tmp);
+}
